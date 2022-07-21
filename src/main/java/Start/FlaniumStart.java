74131c631163daf05e530bc7ecfeb2fe57c04624
AdminTst <max90003@gmail.com>
2022-07-21 18:25:51 +0300

    запуск ztn

--- src/main/java/Start/FlaniumStart.java
+++ src/main/java/Start/FlaniumStart.java
@@ -1,15 +1,21 @@
 package Start;
 
+import FlaNium.WinAPI.elements.Button;
+import FlaNium.WinAPI.elements.ComboBox;
+import FlaNium.WinAPI.elements.TabItem;
+import FlaNium.WinAPI.elements.Window;
 import FlaNium.WinAPI.webdriver.DesktopOptions;
 import FlaNium.WinAPI.webdriver.FlaNiumDriver;
 import FlaNium.WinAPI.webdriver.FlaNiumDriverService;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 
 public class FlaniumStart {
-    static String DRIVER_PATH = "C:\\Users\\Admin\\IdeaProjects\\Flat\\src\\main\\resources\\FlaNium.Desktop.Driver-v1.6.0/FlaNium.Driver.exe";
-    static String APP_PATH = "C:\\Program Files\\7-Zip/7zFM.exe";
-    static int driverPort = 10000;
+    static String DRIVER_PATH = "C:\\Users\\AdminTst\\IdeaProjects\\FlatTest\\src\\main\\resources\\FlaNium.Desktop.Driver-v1.6.0/FlaNium.Driver.exe";
+    static String APP_PATH = "C:\\Program Files\\Security Code\\Continent ZTN Client/ZtnClient.exe";
+    static int driverPort = 9999;
     public static void main(String[] args) {
         FlaNiumDriverService service = new FlaNiumDriverService.Builder()
                 // Указание пути до драйвера
@@ -25,14 +31,21 @@ public class FlaniumStart {
 // Инициализация приложения:
         DesktopOptions options = new DesktopOptions();
 // Указание пути до тестируемого приложения
-        options.setApplicationPath(String.valueOf(new File(APP_PATH)));
+        options.setApplicationPath(String.valueOf(new  File(APP_PATH)));
 // Задержка после запуска приложения (сек)
-        options.setLaunchDelay(5);
+        options.setLaunchDelay(10);
+
 // Подключение к ранее запущенному экземпляру приложения
         options.setDebugConnectToRunningApp(false);
 
 // Получение экземпляра драйвера приложения
         FlaNiumDriver driver = new FlaNiumDriver(service, options);
+
+        Button window = new Button(driver.findElement(By.xpath(" //Button[last()] ")));
+        window.click();
+        options.setLaunchDelay(10);
+        Button button = new Button(driver.findElement(By.xpath("//ToolBar[1]")));
+        button.click();
 
     }
 }
